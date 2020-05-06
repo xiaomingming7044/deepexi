925ee771141b5eabebd03265a46d0dc82a3cafc0
xiaomingming7044 <704495240@qq.com>
2020-05-06 10:05:15 +0800

    可以访问

--- message-service/message-service-provider/src/main/java/org/hehaoming/config/ApplicationConfiguration.java
+++
@@ -1,9 +0,0 @@
-package org.hehaoming.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Bean;
-
-@Configuration
-public class ApplicationConfiguration {
-    
-}
