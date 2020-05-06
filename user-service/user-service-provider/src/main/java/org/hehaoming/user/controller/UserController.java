c1cc764e4043dd7269ae0cf97333ef9f24eefe3b
xiaomingming7044 <704495240@qq.com>
2020-05-06 15:45:31 +0800

    service

--- user-service/user-service-provider/src/main/java/org/hehaoming/user/controller/UserController.java
+++ user-service/user-service-provider/src/main/java/org/hehaoming/user/controller/UserController.java
@@ -15,11 +15,11 @@ import org.hehaoming.user.domain.query.UpdateUser;
 import org.hehaoming.user.domain.vo.FindLikeUser;
 import org.hehaoming.user.domain.vo.Pagination;
 import org.hehaoming.user.mapper.UserMapper;
+import org.hehaoming.user.service.RabbitMQDemoServiceImpl;
+import org.hehaoming.user.service.UserServiceImpl;
 import org.springframework.beans.BeanUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
@@ -28,7 +28,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("user")
-@Api(tags={"用户增删改查"})
+@Api(tags = {"用户增删改查"})
 @ApiResponses({
         @ApiResponse(code = -2, message = "业务逻辑异常"),
         @ApiResponse(code = -1, message = "校验失败"),
@@ -37,68 +37,47 @@ import java.util.List;
 public class UserController {
 
 
-//    private final RabbitMQDemoServiceImpl mqDemoService;
+    private RabbitMQDemoServiceImpl mqDemoService;
+    private UserServiceImpl userService;
 
     @Resource
     private UserMapper userMapper;
 
-//    @Autowired
-//    public UserController(RabbitMQDemoServiceImpl mqDemoService) {
-//        this.mqDemoService = mqDemoService;
-//    }
 
-//    @GetMapping("greeting")
-//    public String sayHello(String str) {
-//        mqDemoService.produce(str);
-//        return "welcome!";
-//    }
+    @Autowired
+    public UserController(RabbitMQDemoServiceImpl mqDemoService, UserServiceImpl userService) {
+        this.mqDemoService = mqDemoService;
+        this.userService = userService;
+    }
+
+    @GetMapping("greeting")
+    public String sayHello(String str) {
+        mqDemoService.produce(str);
+        return "welcome!";
+    }
 
     @PostMapping("findLikeUser")
     @ApiOperation(value = "模糊查询，无需查询的字段不用传")
     public Pagination<UserDTO> findLikeUser(@RequestBody FindLikeUser findLikeUser) {
-        Page page =  PageHelper.startPage(findLikeUser.getPageNum(),findLikeUser.getPageSize());
-        FindUser findUser = new FindUser();
-        BeanUtils.copyProperties(findLikeUser,findUser);
-        List<UserDTO> userDTOList = userMapper.findLikeUser(findUser);
-
-        return new Pagination(page.getPageNum(), page.getPageSize(), page.getPages(), page.getTotal(), userDTOList);
+        return userService.findLikeUser(findLikeUser);
     }
 
     @PostMapping("addUser")
     @ApiOperation(value = "添加用户")
     public String addUser(@Valid @RequestBody AddUser addUser) throws Exception {
-        if (userMapper.exactFindUser(new FindUser(null, addUser.getPhone(), null, false)).size() >= 1) {
-            throw new Exception(Constant.PHONE_IS_REGIST);
-        }
-        userMapper.saveUser(addUser);
-        return Constant.SUCCESS;
+        return userService.addUser(addUser);
     }
 
     @PostMapping("deleteUser")
     @ApiOperation(value = "根据id批量删除用户")
     public String deleteUser(@RequestBody @NotNull List<Integer> ids) throws Exception {
-        if(ids.size() == 0){
-            throw new Exception(Constant.NO_IDS);
-        }
-        userMapper.deleteUser(ids);
-        return Constant.SUCCESS;
+        return userService.deleteUser(ids);
     }
 
     @PostMapping("updateUser")
     @ApiOperation(value = "根据用户id更改用户信息")
     public String updateUser(@Valid @RequestBody UpdateUser updateUser) throws Exception {
-        List<UserDTO> list = userMapper.exactFindUser(new FindUser(updateUser.getId(),null,null,false));
-        if (list.size() != 1) {
-            throw new Exception(Constant.ID_NO_FOUND);
-        }
-        UserDTO oldUser = list.get(0);
-
-        //改手机号码前，查询新号码是否已被注册
-        if (!oldUser.getPhone().equals(updateUser.getPhone()) && userMapper.exactFindUser(new FindUser(null,updateUser.getPhone(),null,false)).size() > 0) {
-            throw new Exception(Constant.PHONE_IS_REGIST);
-        }
-        userMapper.updateUser(updateUser);
-        return Constant.SUCCESS;
+        return userService.updateUser(updateUser);
     }
 
 }
